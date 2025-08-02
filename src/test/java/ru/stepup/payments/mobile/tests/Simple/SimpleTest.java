fe07342ed1f46b937dce88400c1b07c31e7d34eb
vitklimin <vitklimin@bk.ru>
2025-08-02 17:05:42 +0400

    checks with file paths

---
+++ src/test/java/ru/stepup/payments/mobile/tests/Simple/SimpleTest.java
@@ -0,0 +1,12 @@
+package ru.stepup.payments.mobile.tests.Simple;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    public void simpleTest(){
+        Assertions.assertEquals(1,1);
+    }
+
+}
